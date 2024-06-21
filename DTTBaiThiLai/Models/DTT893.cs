7cfb7a505950d64b062dc78cdbc4ed9bcf698f8b
dothuthuy <dothuthuy1212002@gmail.com>
2024-06-21 15:20:56 +0700

    Bài 1

---
+++ DTTBaiThiLai/Models/DTT893.cs
@@ -0,0 +1,9 @@
+namespace DTTBaiThiLai.Models
+{
+    public class DTT893
+    {
+        public string HoTen {get;set;}
+        public int ID {get;set;}
+        public DateTime Ngaysinh {get;set;}
+    }
+}
